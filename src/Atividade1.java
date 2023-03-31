d041043aa85ba0bfc04ed3e393109b219d1d0cef
Thiago Pereira Da Silva <thiago.silva@venturus.org.br>
2023-03-31 10:44:37 -0300

    Meus arquivos de Animal

---
+++ src/Atividade1.java
@@ -0,0 +1,39 @@
+public class Atividade1 {
+    public static void main(String[] args) {
+//        Animal n = new  Animal();
+        Mamifero m = new Mamifero();
+        Reptil r = new Reptil();
+        Peixe p = new Peixe();
+        Ave a = new  Ave();
+        System.out.println("---------------------------------------");
+
+        m.setPeso(35.3f);
+        m.setCorPelo("Marrom");
+        m.alimentar();
+        m.locomover();
+        m.emitirSom();
+        System.out.println("-----------------------------------------");
+
+        r.setPeso(35.3f);
+        r.setCorEscama("Verde");
+        r.alimentar();
+        r.locomover();
+        r.emitirSom();
+        System.out.println("------------------------------------------");
+
+        p.setPeso(0.35f);
+        p.setCorScama("Cinza");
+        p.alimentar();
+        p.locomover();
+        p.emitirSom();
+        System.out.println("------------------------------------------");
+
+        a.setPeso(0.89f);
+        a.setCorPena("Preta");
+        a.alimentar();
+        a.locomover();
+        a.emitirSom();
+        a.fazerNinho();
+        System.out.println("------------------------------------------");
+    }
+}
